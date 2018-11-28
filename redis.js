b99f45256c016dc6b345e49ba23b3277d598c019
Peter Anderson <pete.anderson@gmail.com>
2018-11-28 11:45:03 +0100

    fixed redis production env

--- redis.js
+++ redis.js
@@ -1,8 +1,17 @@
 const redis = require('redis');
-const client = redis.createClient({
-    host: 'localhost',
-    port: 6379
-});
+let client;
+
+if (process.env.NODE_ENV === 'production') {
+    client = redis.createClient({
+        host: 'ec2-54-209-190-123.compute-1.amazonaws.com',
+        port: 7739
+    });
+} else {
+    client = redis.createClient({
+        host: 'localhost',
+        port: 6379
+    });
+}
 
 const {promisify} = require('util');
 
